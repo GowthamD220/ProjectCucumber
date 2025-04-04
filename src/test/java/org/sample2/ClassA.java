edf9996ec281f2acff381acc8fc714ed046dcc6d
GowthamD220 <gs3197330@gmail.com>
2025-04-04 13:46:25 +0530

    Package 2 created and two classes created

---
+++ src/test/java/org/sample2/ClassA.java
@@ -0,0 +1,18 @@
+package org.sample2;
+
+public class ClassA {
+
+	private void tamilNadu() {
+		System.out.println("A");
+
+	}
+	private void andhra() {
+		System.out.println("B");
+
+	}
+	private void kerala() {
+		System.out.println("C");
+	}
+	
+	
+}
